fd59e08bb72ac57926e582fe63a4d06f1a10df3f
Wendel S. Pereira <netalcode@gmail.com>
2022-03-07 16:32:33 -0300

    implementada a listagem e a selecao de abrigos por id

--- src2/modules/cars/useCases/inputCategory/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryRepository } from "../../repository/implementations/CategoryRepository";
-import { InputCategoryController } from "./inputCategoryController";
-import { InputCategoryUseCase } from "./inputCategoryUseCase";
-
-const categoryRepository = CategoryRepository.getInstance()
-const inputCategoryUseCase = new InputCategoryUseCase(categoryRepository)
-const inputCategoryController = new InputCategoryController(inputCategoryUseCase)
-
-export {inputCategoryController}
